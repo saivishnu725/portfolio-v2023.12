ffa2c3f55e686bf0d95c36a0f66e7b8dca58d9b9
saivishnu725 <saivishnu725@gmail.com>
2024-02-15 09:05:03 +0530

    subdomain tried

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import subdomain from "express-subdomain";
 
 const app = express();
 
@@ -109,25 +110,30 @@ app.get("/", function (req, res) {
     });
 });
 
-// GET: blog's primary page
-app.get("/blog", function (req, res) {
+// GET: blog subdomain
+const blogRouter = express.Router();
+blogRouter.get("/", function (req, res) {
+    console.log("blog page");
     res.render("blogs/blog", { blogs: blogs, socials: socials });
 });
-// GET: blog pages
-app.get("/blog/:title", function (req, res) {
+
+// Register the subdomain middleware
+app.use(subdomain('blogs', blogRouter));
+
+// GET: blog page
+blogRouter.get("/:title", function (req, res) {
     const requestedTitle = req.params.title;
 
     // if the page is available, open it. Or else, send a 404
     if (availableTitle.includes(requestedTitle)) {
         res.send(requestedTitle);
-        // Open the page
-        // res.render("blogs/" + requestedTitle);
     } else {
         //  open a 404 error page
         // TODO: create a todo page
         res.status(404).send(`404 Page Not Found` + `<br /> <a href='/'> Go back home </a>`);
     }
 });
+
 
 // GET: contact page
 app.get("/contact", function (req, res) {
