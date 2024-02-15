1fcd955acd1e0e6a73cb95026e929237a4cbd783
saivishnu725 <saivishnu725@gmail.com>
2024-02-15 03:46:37 +0000

    trying to make pm2 work

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+export default {
+	apps: [
+		{
+			name: 'portfolio-v2023.12',
+			script: 'index.js',
+			exec_mode: 'cluster',
+			instances: 'max',
+			cwd: '/home/theunconcernedape/Programs/portfolio-v2023.12/',
+		},
+	],
+};
